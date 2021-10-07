ffebf66e42c965c21b5564eaf58e4f434e2dff81
MikelTelegin <91802411+MikelTelegin@users.noreply.github.com>
2021-10-07 22:01:36 +0300

    Create homework_3_2.py

---
+++ homework_3_2.py
@@ -0,0 +1,10 @@
+# task 2
+def my_func2():
+    name = input("Введите имя")
+    surname = input("Введите фамилию")
+    year = int(input("Введите год рождения"))
+    city = input("Введите город проживания")
+    email = input("Введите адрес электронной почты")
+    mobile = input("Введите номер мобильного телефона")
+    print(name, " ", surname, " ", year, " ", city, " ", email, " ", mobile)
+my_func2()
